4f728841ccedb1ba69378a06828733b40b6c5c00
Polynomeer <polynomeer@gmail.com>
2021-10-20 14:19:33 +0900

    refactor: Adapt Spring framework to application
    Added @Configuration and @Bean annotations to AppConfig.
    Adapt Spring framework to MemberApp and OrderApp using ApplicationContext.

--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -4,16 +4,22 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
 //        MemberService memberService = new MemberServiceImpl();
+
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
         /**
          * 이전에는 직접 new로 인스턴스를 생성하여 사용했지만,
          * 이제는 AppConfig를 통해서 MemberServiceImpl을 받아온다.
          */
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
