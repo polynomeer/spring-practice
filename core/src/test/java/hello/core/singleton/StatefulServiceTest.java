d426ca468672b449f7999bd8dd4a0d7397477094
Polynomeer <polynomeer@gmail.com>
2021-10-27 21:07:34 +0900

    test: Change to remove singleton problem

--- core/src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ core/src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -15,16 +15,28 @@ class StatefulServiceTest {
         StatefulService statefulService1 = ac.getBean(StatefulService.class);
         StatefulService statefulService2 = ac.getBean(StatefulService.class);
 
+        /*
         // ThreadA: A 사용자가 10000원 주문
         statefulService1.order("userA", 10000);
         // ThreadB: A 사용자가 10000원 주문
         statefulService1.order("userB", 20000);
+
 
         // ThreadA: 사용자 A가 주문 금액 조회
         int price = statefulService1.getPrice();
         System.out.println("price = " + price);
 
         assertThat(statefulService1.getPrice()).isEqualTo(20000);
+        */
+
+        // ThreadA: A 사용자가 10000원 주문
+        int userAPrice = statefulService1.order("userA", 10000);
+        // ThreadB: A 사용자가 10000원 주문
+        int userBPrice = statefulService1.order("userB", 20000);
+
+        // ThreadA: 사용자 A가 주문 금액 조회
+        System.out.println("userAPrice = " + userAPrice);
+        assertThat(userAPrice).isEqualTo(10000);
     }
 
     static class TestConfig {
