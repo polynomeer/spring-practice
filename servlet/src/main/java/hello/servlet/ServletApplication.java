7f35284485c146d0cf9a853510e8aeaa30be78e9
Polynomeer <polynomeer@gmail.com>
2021-11-10 17:38:10 +0900

    feat: Add configuration for testing view resolver

--- servlet/src/main/java/hello/servlet/ServletApplication.java
+++ servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -3,13 +3,21 @@ package hello.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-@ServletComponentScan	// 서블릿을 자동으로 등록해주는 애노테이션
+@ServletComponentScan    // 서블릿을 자동으로 등록해주는 애노테이션
 @SpringBootApplication
 public class ServletApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServletApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServletApplication.class, args);
+    }
 
+    /* 스프링 부트에서는 application.properties 에 설정하면 뷰 리졸버가 자동으로 설정된다.
+    @Bean
+    InternalResourceViewResolver internalResourceViewResolver() {
+        return new InternalResourceViewResolver("/WEB-INF/views", ".jsp");
+    }
+    */
 }
