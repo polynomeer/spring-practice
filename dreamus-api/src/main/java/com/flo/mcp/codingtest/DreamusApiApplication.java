ce6bb7e36a9a360802074254a35f148dc8d4482d
polynomeer <polynomeer@gmail.com>
2021-11-25 22:56:12 +0900

    build: Initialize dreamus-api project and .gitignore

---
+++ dreamus-api/src/main/java/com/flo/mcp/codingtest/DreamusApiApplication.java
@@ -0,0 +1,13 @@
+package com.flo.mcp.codingtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamusApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamusApiApplication.class, args);
+	}
+
+}
---
+++ dreamus-api/src/test/java/com/flo/mcp/codingtest/DreamusApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flo.mcp.codingtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamusApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
