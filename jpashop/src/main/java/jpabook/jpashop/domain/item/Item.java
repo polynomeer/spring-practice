c7f17b1de092f057f0909412582d4b592b7f625e
polynomeer <polynomeer@gmail.com>
2022-01-28 11:18:51 +0900

    fix: Fix to update stockQuantity in Item::removeStock

--- jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -45,5 +45,6 @@ public abstract class Item {
         if (restStock < 0) {
             throw new NotEnoughStockException("need more stock");
         }
+        this.stockQuantity = restStock;
     }
 }
