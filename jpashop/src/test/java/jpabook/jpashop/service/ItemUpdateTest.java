a911c5fc57905bb3ef508b2e392fd207121a0ba5
polynomeer <polynomeer@gmail.com>
2022-01-30 12:23:50 +0900

    test: Add ItemUpdateTest

---
+++ jpashop/src/test/java/jpabook/jpashop/service/ItemUpdateTest.java
@@ -0,0 +1,31 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.item.Book;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+@SpringBootTest
+@Transactional
+public class ItemUpdateTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    public void updateTest() throws Exception {
+        Book book = em.find(Book.class, 1L);
+
+        System.out.println(book);
+
+        // TX
+        book.setName("modified");
+
+        // TX commit
+        // 변경감지 == dirty checking
+        System.out.println(book);
+    }
+ }
