1ea6d4a11f8537cadfe19e8f3a59ad7338a22b6f
polynomeer <polynomeer@gmail.com>
2022-01-31 14:38:12 +0900

    feat: Add force reset logic for orders

--- jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -11,7 +11,6 @@ import java.util.List;
 
 /**
  * XToOne(ManyToOne, OneToOne)
- *
  * [Order]
  * Order -> Member (ManyToOne)
  * Order -> Delivery (OneToOne)
@@ -25,6 +24,10 @@ public class OrderSimpleApiController {
     @GetMapping("/api/v1/simple-orders")
     public List<Order> ordersV1() {
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName();        // Lazy 강제 초기화
+            order.getDelivery().getAddress();   // Lazy 강제 초기화
+        }
         return all;
     }
 }
