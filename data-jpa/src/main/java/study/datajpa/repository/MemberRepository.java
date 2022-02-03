e7774ef8ce94ed674543d0190567b727ad3e6c2e
polynomeer <polynomeer@gmail.com>
2022-02-04 00:42:57 +0900

    feat: Add MemberRepository::findByUsername to use named query

--- data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,6 +1,8 @@
 package study.datajpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import study.datajpa.entity.Member;
 
 import java.util.List;
@@ -12,4 +14,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     List<Member> findHelloBy();
 
     List<Member> findTop3By();
+
+    @Query(name = "Member.findByUsername")
+    List<Member> findByUsername(@Param("username") String username);
 }
