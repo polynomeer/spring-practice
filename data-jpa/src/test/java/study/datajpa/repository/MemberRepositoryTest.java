81cad560acbbfcd8c7adaeebb102bc92d31e4393
polynomeer <polynomeer@gmail.com>
2022-02-04 23:23:54 +0900

    test: Add sample codes to test various methods

--- data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -274,6 +274,8 @@ class MemberRepositoryTest {
 
         // when
         List<Member> members = memberRepository.findAll();
+//        List<Member> members = memberRepository.findMemberFetchJoin();
+//        List<Member> members = memberRepository.findEntityGraphByUsername("member1");
 
         for (Member member : members) {
             System.out.println("member = " + member.getUsername());
