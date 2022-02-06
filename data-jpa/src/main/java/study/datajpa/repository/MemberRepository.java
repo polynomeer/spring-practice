b05a1847de0bbe9ec58978c15bfade1e49853c26
polynomeer <polynomeer@gmail.com>
2022-02-07 00:27:52 +0900

    feat: Add UsernameOnly and projection method for it

--- data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -70,4 +70,6 @@ public interface MemberRepository extends JpaRepository<Member, Long>, MemberRep
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     Member findLockByUsername(String username);
+
+    List<UsernameOnly> findProjectionsByUsername(@Param("username") String username);
 }
