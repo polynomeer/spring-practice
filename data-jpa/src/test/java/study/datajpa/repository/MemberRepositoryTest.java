ba12ee9983d115b9eb6413f0cc488402c7905b49
polynomeer <polynomeer@gmail.com>
2022-02-07 19:31:28 +0900

    test: Add MemberRepositoryTest::nativeQuery

--- data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -447,4 +447,24 @@ class MemberRepositoryTest {
         }
 
     }
+
+
+    @Test
+    public void nativeQuery() {
+        // given
+        Team teamA = new Team("teamA");
+        em.persist(teamA);
+
+        Member member1 = new Member("member1", 0, teamA);
+        Member member2 = new Member("member2", 0, teamA);
+        em.persist(member1);
+        em.persist(member2);
+
+        em.flush();
+        em.clear();
+
+        // when
+        Member result = memberRepository.findByNativeQuery("member1");
+        System.out.println("result = " + result);
+    }
 }
