9b11ba532f62fad82ffa5d8a19615ff67f409d8a
polynomeer <polynomeer@gmail.com>
2022-02-13 00:10:53 +0900

    refactor: Rename StudyDashboard::studyReviews to StudyDashboard::loadReviews

--- refactoring/src/main/java/com/august/refactoring/_01_smell_mysterious_name/_01_before/StudyDashboard.java
+++ refactoring/src/main/java/com/august/refactoring/_01_smell_mysterious_name/_01_before/StudyDashboard.java
@@ -16,7 +16,13 @@ public class StudyDashboard {
 
     private Set<String> reviews = new HashSet<>();
 
-    private void studyReviews(GHIssue issue) throws IOException {
+    /**
+     * 스터디 리뷰 이슈에 작성되어 있는 리뷰어 목록과 리뷰를 읽어옵니다.
+     *
+     * @param issue
+     * @throws IOException
+     */
+    private void loadReviews(GHIssue issue) throws IOException {
         List<GHIssueComment> comments = issue.getComments();
         for (GHIssueComment comment : comments) {
             usernames.add(comment.getUserName());
@@ -38,7 +44,7 @@ public class StudyDashboard {
         GHIssue issue = repository.getIssue(30);
 
         StudyDashboard studyDashboard = new StudyDashboard();
-        studyDashboard.studyReviews(issue);
+        studyDashboard.loadReviews(issue);
         studyDashboard.getUsernames().forEach(System.out::println);
         studyDashboard.getReviews().forEach(System.out::println);
     }
