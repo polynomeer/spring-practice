e63151b4e2334e0b8f98f520e75dd439610793a0
polynomeer <polynomeer@gmail.com>
2022-02-13 01:22:35 +0900

    feat: Add Dashboard, ParticipantDashboard, and ReviewerDashboard

---
+++ refactoring/src/main/java/com/august/refactoring/_02_duplicated_code/Dashboard.java
@@ -0,0 +1,14 @@
+package com.august.refactoring._02_duplicated_code;
+
+import java.io.IOException;
+
+public class Dashboard {
+
+    public static void main(String[] args) throws IOException {
+        ReviewerDashboard reviewerDashboard = new ReviewerDashboard();
+        reviewerDashboard.printReviewers();
+
+        ParticipantDashboard participantDashboard = new ParticipantDashboard();
+        participantDashboard.printParticipants(15);
+    }
+}
