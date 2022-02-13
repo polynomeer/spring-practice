1a4562dd5dde1c0d6ce2a076e8aff7c158c5b142
polynomeer <polynomeer@gmail.com>
2022-02-13 15:49:43 +0900

    feat: Add Shipment and fields for Order

--- refactoring/src/main/java/com/august/refactoring/_04_long_parameter_list/Order.java
+++ refactoring/src/main/java/com/august/refactoring/_04_long_parameter_list/Order.java
@@ -1,14 +1,27 @@
 package com.august.refactoring._04_long_parameter_list;
 
+import java.time.LocalDate;
+
 public class Order {
 
-    private int quantity;
-
-    private double itemPrice;
+    private final int quantity;
+    private final double itemPrice;
+    private final LocalDate placedOn;
+    private final String deliveryState;
 
     public Order(int quantity, double itemPrice) {
+        this(quantity, itemPrice, null, null);
+    }
+
+    public Order(LocalDate placedOn, String deliveryState) {
+        this(0, 0, placedOn, deliveryState);
+    }
+
+    public Order(int quantity, double itemPrice, LocalDate placedOn, String deliveryState) {
         this.quantity = quantity;
         this.itemPrice = itemPrice;
+        this.placedOn = placedOn;
+        this.deliveryState = deliveryState;
     }
 
     public double finalPrice() {
@@ -22,5 +35,13 @@ public class Order {
 
     private double discountedPrice(double basePrice) {
         return discountLevel() == 2 ? basePrice * 0.9 : basePrice * 0.95;
+    }
+
+    public LocalDate getPlacedOn() {
+        return placedOn;
+    }
+
+    public String getDeliveryState() {
+        return deliveryState;
     }
 }
