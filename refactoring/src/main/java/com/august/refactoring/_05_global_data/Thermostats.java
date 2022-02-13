2568bd2adaf8db3ff3c98f54fdd9b5f12a0733c2
polynomeer <polynomeer@gmail.com>
2022-02-13 16:14:23 +0900

    feat: Add Home and Thermostats to study global data

---
+++ refactoring/src/main/java/com/august/refactoring/_05_global_data/Thermostats.java
@@ -0,0 +1,13 @@
+package com.august.refactoring._05_global_data;
+
+public class Thermostats {
+
+    public static Integer targetTemperature = 70;
+
+    public static Boolean heating = true;
+
+    public static Boolean cooling = false;
+
+    public static Boolean fahrenheit = true;
+
+}
