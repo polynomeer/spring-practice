8f33f0e1541c5aadd79d714a5b2913086efc90a7
polynomeer <polynomeer@gmail.com>
2022-02-13 16:05:58 +0900

    feat: Add classes for combining functions into class example

---
+++ refactoring/src/main/java/com/august/refactoring/_04_long_parameter_list/Participant.java
@@ -0,0 +1,15 @@
+package com.august.refactoring._04_long_parameter_list;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public record Participant(String username, Map<Integer, Boolean> homework) {
+    public Participant(String username) {
+        this(username, new HashMap<>());
+    }
+
+    public void setHomeworkDone(int index) {
+        this.homework.put(index, true);
+    }
+
+}
