f32e17d397f1171bc379e5ee506e76a7cd466dc1
polynomeer <polynomeer@gmail.com>
2022-02-19 01:23:47 +0900

    refactor: Introduce variable for enrichReading(reading)

--- refactoring/src/main/java/com/august/refactoring/_06_mutable_data/_22_combine_functions_into_transform/Client2.java
+++ refactoring/src/main/java/com/august/refactoring/_06_mutable_data/_22_combine_functions_into_transform/Client2.java
@@ -6,8 +6,9 @@ public class Client2 extends ReadingClient {
     private double taxableCharge;
 
     public Client2(Reading reading) {
-        this.base = enrichReading(reading).baseCharge();
-        this.taxableCharge = enrichReading(reading).taxableCharge();
+        EnrichReading enrichReading = enrichReading(reading);
+        this.base = enrichReading.baseCharge();
+        this.taxableCharge = enrichReading.taxableCharge();
     }
 
     public double getBase() {
