af5c8d159db51f677675a55638fd62257de05e92
polynomeer <polynomeer@gmail.com>
2022-02-20 22:23:50 +0900

    feat: Add replace primitive with object examples

---
+++ refactoring/src/main/java/com/august/refactoring/_11_primitive_obsession/_30_replace_primitive_with_object/Order.java
@@ -0,0 +1,14 @@
+package com.august.refactoring._11_primitive_obsession._30_replace_primitive_with_object;
+
+public class Order {
+
+    private String priority;
+
+    public Order(String priority) {
+        this.priority = priority;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+}
