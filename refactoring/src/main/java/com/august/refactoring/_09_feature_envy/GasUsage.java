d739bc576ab10d8059588420a9ae5616346e08d0
polynomeer <polynomeer@gmail.com>
2022-02-20 14:52:55 +0900

    feat: Add feature envy examples

---
+++ refactoring/src/main/java/com/august/refactoring/_09_feature_envy/GasUsage.java
@@ -0,0 +1,21 @@
+package com.august.refactoring._09_feature_envy;
+
+public class GasUsage {
+
+    private double amount;
+
+    private double pricePerUnit;
+
+    public GasUsage(double amount, double pricePerUnit) {
+        this.amount = amount;
+        this.pricePerUnit = pricePerUnit;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public double getPricePerUnit() {
+        return pricePerUnit;
+    }
+}
