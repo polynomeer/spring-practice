fc88d8cde811af30f2fae5128ac4b60c40d4693d
polynomeer <polynomeer@gmail.com>
2022-02-21 19:32:13 +0900

    refactor: Replace loop with pipeline by using stream

--- refactoring/src/main/java/com/august/refactoring/_13_loop/_33_replace_loop_with_pipeline/Author.java
+++ refactoring/src/main/java/com/august/refactoring/_13_loop/_33_replace_loop_with_pipeline/Author.java
@@ -1,13 +1,13 @@
 package com.august.refactoring._13_loop._33_replace_loop_with_pipeline;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class Author {
 
-    private String company;
-
-    private String twitterHandle;
+    private final String company;
+    private final String twitterHandle;
 
     public Author(String company, String twitterHandle) {
         this.company = company;
@@ -15,15 +15,10 @@ public class Author {
     }
 
     static public List<String> TwitterHandles(List<Author> authors, String company) {
-        var result = new ArrayList<String>();
-        for (Author a : authors) {
-            if (a.company.equals(company)) {
-                var handle = a.twitterHandle;
-                if (handle != null)
-                    result.add(handle);
-            }
-        }
-        return result;
+        return authors.stream().filter(author -> author.company.equals(company))
+                .map(author -> author.twitterHandle)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
     }
 
 }
