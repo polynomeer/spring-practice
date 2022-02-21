be40daace4a3c07e868798409515057b60a01090
polynomeer <polynomeer@gmail.com>
2022-02-21 19:11:28 +0900

    refactor: Modify from switch statement to expression

--- refactoring/src/main/java/com/august/refactoring/_12_repeated_swtiches/SwitchImprovements.java
+++ refactoring/src/main/java/com/august/refactoring/_12_repeated_swtiches/SwitchImprovements.java
@@ -3,20 +3,11 @@ package com.august.refactoring._12_repeated_swtiches;
 public class SwitchImprovements {
 
     public int vacationHours(String type) {
-        int result;
-        switch (type) {
-            case "full-time":
-                result = 120;
-                break;
-            case "part-time":
-                result = 80;
-                break;
-            case "temporal":
-                result = 32;
-                break;
-            default:
-                result = 0;
-        }
-        return result;
+        return switch (type) {
+            case "full-time" -> 120;
+            case "part-time" -> 80;
+            case "temporal" -> 32;
+            default -> 0;
+        };
     }
 }
