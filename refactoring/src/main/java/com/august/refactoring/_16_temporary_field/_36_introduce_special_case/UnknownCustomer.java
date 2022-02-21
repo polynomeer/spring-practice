74630a2be703bff7abaa9ed7a695a4662ad0ec3d
polynomeer <polynomeer@gmail.com>
2022-02-21 20:50:41 +0900

    refactor: Move checking special case logic to UnknownCustomer

--- refactoring/src/main/java/com/august/refactoring/_16_temporary_field/_36_introduce_special_case/UnknownCustomer.java
+++ refactoring/src/main/java/com/august/refactoring/_16_temporary_field/_36_introduce_special_case/UnknownCustomer.java
@@ -3,7 +3,7 @@ package com.august.refactoring._16_temporary_field._36_introduce_special_case;
 public class UnknownCustomer extends Customer {
 
     public UnknownCustomer() {
-        super("unknown", new BasicBillingPlan(), null);
+        super("unknown", new BasicBillingPlan(), new NullPaymentHistory());
     }
 
     @Override
@@ -15,5 +15,5 @@ public class UnknownCustomer extends Customer {
     public String getName() {
         return "occupant";
     }
-    
+
 }
