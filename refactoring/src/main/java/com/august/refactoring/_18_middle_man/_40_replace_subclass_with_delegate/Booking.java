d37b7fb9669ea5370733249aa86781320bbcf330
polynomeer <polynomeer@gmail.com>
2022-02-21 21:32:59 +0900

    refactor: Add PremiumDelegate to remove PremiumBooking

--- refactoring/src/main/java/com/august/refactoring/_18_middle_man/_40_replace_subclass_with_delegate/Booking.java
+++ refactoring/src/main/java/com/august/refactoring/_18_middle_man/_40_replace_subclass_with_delegate/Booking.java
@@ -6,12 +6,23 @@ import java.time.LocalDateTime;
 public class Booking {
 
     protected Show show;
-
     protected LocalDateTime time;
+    protected PremiumDelegate premiumDelegate;
 
     public Booking(Show show, LocalDateTime time) {
         this.show = show;
         this.time = time;
+    }
+
+    public static Booking createBook(Show show, LocalDateTime time) {
+        return new Booking(show, time);
+    }
+
+    public static Booking createPremiumBooking(Show show, LocalDateTime time, PremiumExtra extra) {
+        PremiumBooking booking = new PremiumBooking(show, time, extra);
+        // PremiumDelegate로 모두 옮기고 PremiumBooking을 삭제하기 위한 중간과정에 필요한 장치
+        booking.premiumDelegate = new PremiumDelegate(booking, extra);
+        return new PremiumBooking(show, time, extra);
     }
 
     public boolean hasTalkback() {
---
+++ refactoring/src/main/java/com/august/refactoring/_18_middle_man/_40_replace_subclass_with_delegate/PremiumDelegate.java
@@ -0,0 +1,12 @@
+package com.august.refactoring._18_middle_man._40_replace_subclass_with_delegate;
+
+public class PremiumDelegate {
+
+    private Booking host;
+    private PremiumExtra extra;
+
+    public PremiumDelegate(Booking host, PremiumExtra extra) {
+        this.host = host;
+        this.extra = extra;
+    }
+}
