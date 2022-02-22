3d4d46011344474450d4d95ae0cce0b3b262320a
polynomeer <polynomeer@gmail.com>
2022-02-22 19:44:20 +0900

    refactor: Extract superclass Party

--- refactoring/src/main/java/com/august/refactoring/_20_large_class/_41_extract_superclass/Department.java
+++ refactoring/src/main/java/com/august/refactoring/_20_large_class/_41_extract_superclass/Department.java
@@ -2,11 +2,13 @@ package com.august.refactoring._20_large_class._41_extract_superclass;
 
 import java.util.List;
 
-public class Department {
-
-    private String name;
+public class Department extends Party {
 
     private List<Employee> staff;
+
+    public Department(String name) {
+        super(name);
+    }
 
     public String getName() {
         return name;
@@ -16,15 +18,12 @@ public class Department {
         return staff;
     }
 
-    public double totalMonthlyCost() {
-        return this.staff.stream().mapToDouble(e -> e.getMonthlyCost()).sum();
-    }
-
-    public double totalAnnualCost() {
-        return this.totalMonthlyCost() * 12;
-    }
-
     public int headCount() {
         return this.staff.size();
+    }
+
+    @Override
+    public double monthlyCost() {
+        return this.staff.stream().mapToDouble(Employee::monthlyCost).sum();
     }
 }
