449bcfac7546a201ec4cd769e6495611fe08d37f
polynomeer <polynomeer@gmail.com>
2022-02-22 20:14:18 +0900

    refactor: Convert to java record

--- refactoring/src/test/java/com/august/refactoring/_22_data_class/_42_encapsulate_record/OrganizationTest.java
+++ refactoring/src/test/java/com/august/refactoring/_22_data_class/_42_encapsulate_record/OrganizationTest.java
@@ -8,7 +8,6 @@ class OrganizationTest {
 
     @Test
     void name() {
-        Organization organization = new Organization();
-        organization.name = "august"; // 직접적인 접근이 가능
+        Organization organization = new Organization("August", "Korea");
     }
 }
