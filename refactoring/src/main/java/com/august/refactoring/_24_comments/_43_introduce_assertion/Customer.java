69ddc7c89165cf0656c92c1ecf0b7a4e94c22ef5
polynomeer <polynomeer@gmail.com>
2022-02-22 20:30:52 +0900

    refactor: Add exception checking to Customer::setDiscountRate

--- refactoring/src/main/java/com/august/refactoring/_24_comments/_43_introduce_assertion/Customer.java
+++ refactoring/src/main/java/com/august/refactoring/_24_comments/_43_introduce_assertion/Customer.java
@@ -13,7 +13,10 @@ public class Customer {
     }
 
     public void setDiscountRate(Double discountRate) {
-        assert discountRate != null && discountRate > 0;
+//        assert discountRate != null && discountRate > 0;
+        if (discountRate != null && discountRate > 0) {
+            throw new IllegalArgumentException(discountRate + " can't be minus.");
+        }
         this.discountRate = discountRate;
     }
 }
