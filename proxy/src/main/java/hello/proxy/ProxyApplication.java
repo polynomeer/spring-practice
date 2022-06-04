fc6696446243d25bbec42f61c3df6d1667cc0e9d
polynomeer <polynomeer@gmail.com>
2022-06-04 20:38:33 +0900

    feat: Adapt log trace by using interface based proxy

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -2,16 +2,26 @@ package hello.proxy;
 
 import hello.proxy.config.AppV1Config;
 import hello.proxy.config.AppV2Config;
+import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
+import hello.proxy.trace.logtrace.LogTrace;
+import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-@Import({AppV1Config.class, AppV2Config.class}) // 수동으로 컴포넌트 스캔 대상 지정
+//@Import({AppV1Config.class, AppV2Config.class}) // 수동으로 컴포넌트 스캔 대상 지정
+@Import(InterfaceProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") // 주의: 자동 컴포너트 스캔의 범위를 제한
 public class ProxyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProxyApplication.class, args);
+    }
+
+    @Bean
+    public LogTrace logTrace() {
+        return new ThreadLocalLogTrace();
     }
 
 }
