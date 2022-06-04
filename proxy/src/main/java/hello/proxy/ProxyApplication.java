ed0d0053450368a62dcbc257578f3617541158a3
polynomeer <polynomeer@gmail.com>
2022-06-04 11:05:03 +0900

    build: Add V2 configuration bean and import it

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,16 +1,17 @@
 package hello.proxy;
 
 import hello.proxy.config.AppV1Config;
+import hello.proxy.config.AppV2Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import(AppV1Config.class) // 수동으로 컴포넌트 스캔 대상 지정
+@Import({AppV1Config.class, AppV2Config.class}) // 수동으로 컴포넌트 스캔 대상 지정
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") // 주의: 자동 컴포너트 스캔의 범위를 제한
 public class ProxyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProxyApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProxyApplication.class, args);
+    }
 
 }
