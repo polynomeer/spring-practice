d920d50038a241347bfcd13aafb6067e55c2171f
polynomeer <polynomeer@gmail.com>
2022-12-31 11:08:09 +0900

    feat: Add template and controller for showing article

--- firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -6,8 +6,12 @@ import com.example.firstproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
 
 @Controller
 @Slf4j // 로깅을 위한 애너테이션
@@ -34,5 +38,19 @@ public class ArticleController {
         log.info("{}", saved);
 
         return "";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id, Model model) {
+        log.info("id : {}", id);
+
+        // 1. id로 데이터를 가져온다.
+        Article article = articleRepository.findById(id).orElse(null);
+
+        // 2. 가져온 데이터를 모델에 등록한다.
+        model.addAttribute("article", article);
+
+        // 3. 보여줄 페이지를 설정한다.
+        return "articles/show";
     }
 }
