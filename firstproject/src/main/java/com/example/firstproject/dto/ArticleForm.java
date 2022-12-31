6db960170433df224289e88b7302e3ce46eb0101
polynomeer <polynomeer@gmail.com>
2022-12-31 10:27:52 +0900

    feat: Add template and controller for creating article

---
+++ firstproject/src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -0,0 +1,20 @@
+package com.example.firstproject.dto;
+
+public class ArticleForm {
+
+    private final String title;
+    private final String content;
+
+    public ArticleForm(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "ArticleForm{" +
+                "title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
