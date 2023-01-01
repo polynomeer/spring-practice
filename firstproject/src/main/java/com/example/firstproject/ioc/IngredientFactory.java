182d08d6d2fa19111f0e4665ff642e7ff22e47d6
polynomeer <polynomeer@gmail.com>
2023-01-01 23:53:04 +0900

    refactor: Inject IngredientFactory using spring annotation @Component

--- firstproject/src/main/java/com/example/firstproject/ioc/IngredientFactory.java
+++ firstproject/src/main/java/com/example/firstproject/ioc/IngredientFactory.java
@@ -1,5 +1,8 @@
 package com.example.firstproject.ioc;
 
+import org.springframework.stereotype.Component;
+
+@Component // 해당 클래스를 객체로 만들고, 이를 IoC 컨테이너에 등록
 public class IngredientFactory {
 
     public Ingredient get(String menu) {
--- firstproject/src/test/java/com/example/firstproject/ioc/ChefTest.java
+++ firstproject/src/test/java/com/example/firstproject/ioc/ChefTest.java
@@ -1,10 +1,16 @@
 package com.example.firstproject.ioc;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class ChefTest {
+
+    @Autowired
+    IngredientFactory ingredientFactory;
 
     @Test
     void 돈가스_요리하기() {
