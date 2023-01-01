db90581352ba6529a85398ae18df85955c849917
polynomeer <polynomeer@gmail.com>
2023-01-01 20:42:24 +0900

    feat: Add Comment and CommentRepository

---
+++ firstproject/src/main/java/com/example/firstproject/entity/Comment.java
@@ -0,0 +1,33 @@
+package com.example.firstproject.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne // 해당 댓글 엔티티 여러개가 하나의 Article에 연관된다.
+    @JoinColumn(name = "article_id") // article_id 컬럼에 Article의 대표값을 저장한다.
+    private Article article;
+
+    @Column
+    private String nickname;
+
+    @Column
+    private String body;
+
+    public void setArticle(Article article) {
+        this.article = article;
+    }
+}
